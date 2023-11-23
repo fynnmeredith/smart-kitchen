61dbc971d62ef5c42e127d5578a9a727bca63f1b
fmh06 <fynn.meredith@sky.uk>
2023-11-23 16:01:53 +0000

    test none dynamic routing issue

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,15 +4,18 @@ import Markdown from 'markdown-to-jsx';
 
 const getPostContent = (slug: string) => {
   const folder = 'posts/';
+  console.log({ slug });
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, 'utf8');
   const matterContent = matter(content);
+  console.log({ matterContent });
   return matterContent;
 };
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;
   const postContent = getPostContent(slug);
+  console.log({ postContent });
 
   return (
     <div>
--- app/posts/hello/page.tsx
+++ app/posts/hello/page.tsx
@@ -1,12 +1,22 @@
+import fs from 'fs';
+import matter from 'gray-matter';
 import Markdown from 'markdown-to-jsx';
 
-const markdownStuff = '## heading';
+const getPostContent = () => {
+  const folder = 'posts/';
+  const file = `${folder}The-Best-Air-Fryers-2023.md`;
+  const content = fs.readFileSync(file, 'utf8');
+  const matterContent = matter(content);
+  return matterContent;
+};
 
 const HelloPage = () => {
+  const content = getPostContent();
+  console.log({ content });
   return (
     <div>
       <h2 className="">HELLO</h2>
-      <Markdown>{markdownStuff}</Markdown>
+      <Markdown>{content.content}</Markdown>
     </div>
   );
 };
